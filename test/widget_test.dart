8a6c6e0357c6f184bd51a6ae5ce89d3ef4c0533e
Ahmad Einieh <abomoaz3375@gmail.com>
2022-09-04 15:55:03 +0300

    new version

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:yfdownloader/main.dart';
+import 'package:youtube_downloader/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
